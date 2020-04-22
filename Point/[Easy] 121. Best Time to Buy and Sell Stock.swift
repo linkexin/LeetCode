6949c572ed1a9842462f5c90e9f9c551f49151ee
xin <xin@zhihu.com>
2020-04-22 23:21:18 +0800

    [Easy] 121. Best Time to Buy and Sell Stock

---
+++ Point/[Easy] 121. Best Time to Buy and Sell Stock.swift
@@ -0,0 +1,39 @@
+// O(n²)
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        if prices.count <= 1 {
+            return 0
+        }
+        var tMax = 0
+        for i in 0...prices.count - 2 {
+            var sMax = 0
+            for j in i + 1...prices.count - 1 {
+                sMax = max(prices[j] - prices[i], sMax)
+            }
+            tMax = max(sMax, tMax)
+        }
+        return tMax
+    }
+}
+
+// O(n)
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        if prices.count <= 1 {
+            return 0
+        }
+        var tMax = 0
+        var minValue = Int.max
+        for i in 0...prices.count - 1 {
+            if minValue > prices[i] {
+                minValue = prices[i]
+            } else {
+                tMax = max(tMax, prices[i] - minValue)
+            }
+        }
+        return tMax
+    }
+}
+
+let s = Solution()
+s.maxProfit([1, 2, 3, 4,7])
