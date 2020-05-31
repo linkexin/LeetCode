f096bd465475d0f062ca7a375c80d8abadea1e34
linkexin <qdulkx@163.com>
2020-05-31 22:31:44 +0800

    simply

--- Sort/[Medium] 451. Sort Characters By Frequency.swift
+++ Sort/[Medium] 451. Sort Characters By Frequency.swift
@@ -1,3 +1,4 @@
+// faster than 92.65%
 class Solution {
     // 快排
     func sort(_ sta: Int, _ end: Int, _ arr: inout [Character], _ dic: [Character: Int]) {
@@ -36,6 +37,31 @@ class Solution {
         }
         var arr = Array(dic.keys)
         sort(0, arr.count - 1, &arr, dic)
+        
+        var string = ""
+        for c in arr {
+            for _ in 0 ..< (dic[c] ?? 0) {
+                string.append(c)
+            }
+        }
+        return string
+    }
+}
+
+// 用系统的 sort 方法简化写法
+// faster than 97.87%
+class Solution1 {
+    func frequencySort(_ s: String) -> String {
+        // 先循环一次把每个字符出现的次数统计出来
+        // 后续再根据字典中字符对应的出现次数来排序
+        var dic = [Character: Int]()
+        for c in s {
+            dic[c, default: 0] += 1
+        }
+        var arr = Array(dic.keys)
+        arr.sort { (c1, c2) -> Bool in
+            return dic[c1] ?? 0 > dic[c2] ?? 0
+        }
         
         var string = ""
         for c in arr {
