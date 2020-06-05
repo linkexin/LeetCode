8574b227c191dfdf2299cd9c353dac569612d394
linkexin <qdulkx@163.com>
2020-06-06 01:03:35 +0800

    [Hard] 23. Merge k Sorted Lists 🤔

--- 分治/[Hard] 23. Merge k Sorted Lists 🤔.swift
+++ 分治/[Hard] 23. Merge k Sorted Lists 🤔.swift
@@ -43,3 +43,47 @@ class Solution {
         return head.next
     }
 }
+
+// 分治法
+// faster than 22.54%
+class Solution1 {
+    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+        return recursion(lists, 0, lists.count - 1)
+    }
+    
+    func recursion(_ list: [ListNode?], _ begin: Int, _ end: Int) -> ListNode? {
+        guard begin <= end else {
+            return nil
+        }
+        if begin == end {
+            return list[begin]
+        }
+        let mid = (begin + end) / 2
+        let l = recursion(list, begin, mid)
+        let r = recursion(list, mid + 1, end)
+        return mergeTwoLists(l, r)
+    }
+    
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let head = ListNode()
+        var p1 = l1, p2 = l2, tail: ListNode? = head
+        while p1 != nil && p2 != nil {
+            if (p1?.val) ?? 0 <= (p2?.val) ?? 0 {
+                tail?.next = p1
+                p1 = p1?.next
+            } else {
+                tail?.next = p2
+                p2 = p2?.next
+            }
+            tail = tail?.next
+        }
+        if p1 != nil {
+            tail?.next = p1
+        }
+        if p2 != nil {
+            tail?.next = p2
+        }
+        return head.next
+    }
+}
+
