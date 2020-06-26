358a51cc27e048228bdb1d365b3a8a93d2c180da
linkexin <qdulkx@163.com>
2020-06-26 15:50:25 +0800

    [Hard] 51. N-Queens

--- Search/[Hard] 51. N-Queens.swift
+++ Search/[Hard] 51. N-Queens.swift
@@ -42,3 +42,45 @@ class Solution {
         }
     }
 }
+
+// faster than 74.51%
+// 稍做优化，每次遍历结束就直接存储可以直接输出的答案，省去最后的 formatResult 操作
+class Solution1 {
+    var result = [[String]]()
+    var column = Set<Int>()
+    var pie = Set<Int>()
+    var na = Set<Int>()
+    
+    func solveNQueens(_ n: Int) -> [[String]] {
+        var state = Array(repeating: Array(repeating: ".", count: n), count: n)
+        solve(n, 0, &state)
+        return result
+    }
+    
+    func solve(_ n: Int, _ row: Int, _ currentState: inout [[String]]) {
+        if row == n {
+            // 把二维数组转成一维数组
+            result.append(currentState.map {
+                $0.reduce("", {
+                    return $0 + $1
+                })
+            })
+            return
+        }
+        for c in 0 ..< n {
+            if !column.contains(c) && !pie.contains(row + c) && !na.contains(row - c) {
+                column.insert(c)
+                pie.insert(row + c)
+                na.insert(row - c)
+                currentState[row][c] = "Q"
+                
+                solve(n, row + 1, &currentState)
+                
+                column.remove(c)
+                pie.remove(row + c)
+                na.remove(row - c)
+                currentState[row][c] = "."
+            }
+        }
+    }
+}
