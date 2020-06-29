3fe52293b9e14cf5537c721f4a33b99cb0d1ed3f
linkexin <qdulkx@163.com>
2020-06-30 00:22:02 +0800

    [Easy] 69. Sqrt(x) 🤔

--- Binary Search/[Easy] 69. Sqrt(x) 🤔.swift
+++ Binary Search/[Easy] 69. Sqrt(x) 🤔.swift
@@ -26,3 +26,30 @@ class Solution {
         return result
     }
 }
+
+// Int 型的不太实用，写了个 Double 类型的，并且传入一个精读，右边界从 x 开始
+class Solution {
+    func mySqrt(_ x: Double, _ p: Double) -> Double {
+        guard x > 1 else {
+            return x
+        }
+        return dichotomy(x, p)
+    }
+    
+    func dichotomy(_ x: Double, _ p: Double) -> Double {
+        // right 不能从 x/2 开始了
+        var left = 0.0, right = x
+        var result = 0.0
+        // 一旦左右边界之间的差值小于所要求的精读，就结束循环
+        while right - left >= p {
+            let mid = left + (right - left) / 2
+            if mid * mid < x {
+                result = mid
+                left = mid // 因为是浮点数，所以不能 + 1
+            } else {
+                right = mid
+            }
+        }
+        return result
+    }
+}
