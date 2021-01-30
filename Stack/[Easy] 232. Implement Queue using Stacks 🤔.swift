08b79f05c306413319fe5aa2c2f3ef6c65975d8a
linkexin <qdulkx@163.com>
2021-01-30 16:04:31 +0800

    [Easy] 232. Implement Queue using Stacks

--- Stack/[Easy] 232. Implement Queue using Stacks 🤔.swift
+++ Stack/[Easy] 232. Implement Queue using Stacks 🤔.swift
@@ -1,4 +1,4 @@
-// 用两个栈实现队列，
+// 用两个栈实现队列
 class MyQueue {
     // 定义一个输入栈，一个输出栈，push 对输入栈操作，pop 和 peek 对输出栈操作
     var inputStack = [Int]()
@@ -52,3 +52,58 @@ let ret_2: Int = obj.pop()
 let ret_3: Int = obj.peek()
 let ret_4: Bool = obj.empty()
 print("\(ret_2), \(ret_3), \(ret_4)")
+
+
+// 2021.01.30 版本
+// 首先实现一个 pure stack，封装好 stack 特有的操作
+class Stack {
+    var arr = [Int]()
+    func push(_ x: Int) {
+        arr.append(x)
+    }
+    func pop() -> Int {
+        return arr.removeLast()
+    }
+    func count() -> Int {
+        return arr.count
+    }
+}
+
+class MyQueue {
+    var stack = Stack()
+    
+    /** Initialize your data structure here. */
+    init() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    // 在 push 阶段就数据按照 queue 的规则排列好
+    func push(_ x: Int) {
+        let tempStack = Stack()
+        for _ in 0 ..< stack.count() {
+            tempStack.push(stack.pop())
+        }
+        stack.push(x)
+        for _ in 0 ..< tempStack.count() {
+            stack.push(tempStack.pop())
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    func pop() -> Int {
+        return stack.pop()
+    }
+    
+    /** Get the front element. */
+    func peek() -> Int {
+        let peek = stack.pop()
+        stack.push(peek)
+        return peek
+    }
+    
+    /** Returns whether the queue is empty. */
+    func empty() -> Bool {
+        return stack.count() == 0
+    }
+}
