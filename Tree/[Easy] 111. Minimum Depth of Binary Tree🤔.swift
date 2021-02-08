befe2d792ec770b61b5d07fa616abaeddb4d851a
linkexin <qdulkx@163.com>
2021-02-08 18:02:31 +0800

    [Easy] 111. Minimum Depth of Binary Treee bfs

--- Tree/[Easy] 111. Minimum Depth of Binary Tree🤔.swift
+++ Tree/[Easy] 111. Minimum Depth of Binary Tree🤔.swift
@@ -33,6 +33,37 @@ class Solution {
     }
 }
 
+// 2020.02.08 bfs 版本，感觉最小深度用 bfs 做更符合思维习惯
+class Solution {
+    func minDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var queue = [TreeNode]()
+        queue.append(root)
+        
+        var level = 1
+        while queue.count > 0 {
+            let levelNode = queue
+            queue = [TreeNode]() // array 是值类型
+            for node in levelNode {
+                // 找到第一个叶子节点，直接返回当前 level
+                if node.left == nil && node.right == nil {
+                    return level
+                }
+                if let l = node.left {
+                    queue.insert(l, at: 0)
+                }
+                if let r = node.right {
+                    queue.insert(r, at: 0)
+                }
+            }
+            level += 1
+        }
+        return level
+    }
+}
+
 let l1 = TreeNode(3)
 let l2 = TreeNode(5)
 l1.left = l2
