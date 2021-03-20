4e7a02f4a1eb3a734aafc2ff58b6ccf9d4bbe9ae
linkexin <qdulkx@163.com>
2021-03-21 00:55:41 +0800

    [Easy] 234. Palindrome Linked List

--- Slow Fast Point/[Easy] 234. Palindrome Linked List.swift
+++ Slow Fast Point/[Easy] 234. Palindrome Linked List.swift
@@ -23,3 +23,41 @@ class Solution {
         return true
     }
 }
+
+// 快慢指针，想要完全写对并不容易= =
+class Solution {
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var fast = head, slow = head
+        var pre: ListNode? = nil, cur = head, next = head?.next
+        // 反转前半段链表
+        // 注意这里的 while 判断
+        while fast != nil && fast?.next != nil {
+            // 先移动快慢指针
+            fast = fast?.next?.next
+            slow = slow?.next
+            // 再反转节点
+            cur?.next = pre
+            pre = cur
+            cur = next
+            next = next?.next
+        }
+        
+        // 奇数个节点，最终那一个不需要比较，slow 需要往后移一位
+        if fast != nil {
+            slow = slow?.next
+        }
+        
+        // 前半段和后半段逐一比较
+        // 注意这里用 slow 和 pre 两个指针一个个比较
+        // 因为前半段已经反转了，所以 pre 顺着 next 指针一直往后，其实是从中间节点倒着往回走
+        // 正好可以和 slow.next 对应上，感觉好妙
+        while slow != nil {
+            if pre?.val != slow?.val {
+                return false
+            }
+            pre = pre?.next
+            slow = slow?.next
+        }
+        return true
+    }
+}
